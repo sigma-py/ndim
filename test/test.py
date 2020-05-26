1c7a43f742e7587ed0eb51d99dccd5cae3f85fba
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-26 19:09:11 +0200

    separate out nsphere test

--- test/test.py
+++ test/test.py
@@ -5,38 +5,6 @@ import pytest
 tol = 1.0e-10
 
 
-@pytest.mark.parametrize("n", range(10))
-def test_nball(n):
-    def rec(n):
-        assert n >= 0
-        if n == 0:
-            return 1
-        elif n == 1:
-            return 2
-        return rec(n - 2) * 2 * pi / n
-
-    def closed(n):
-        return sqrt(pi) ** n / gamma(n / 2 + 1)
-
-    assert abs(closed(n) - rec(n)) < abs(closed(n)) * tol
-
-
-@pytest.mark.parametrize("n", range(1, 10))
-def test_nsphere(n):
-    def rec(n):
-        assert n >= 1
-        if n == 1:
-            return 2
-        elif n == 2:
-            return 2 * pi
-        return rec(n - 2) * 2 * pi / (n - 2)
-
-    def closed(n):
-        return n * sqrt(pi) ** n / gamma(n / 2 + 1)
-
-    assert abs(closed(n) - rec(n)) < abs(closed(n)) * tol
-
-
 @pytest.mark.parametrize("n", range(1, 10))
 def test_enr(n):
     def rec(n):
---
+++ test/test_nsphere.py
@@ -0,0 +1,37 @@
+import pytest
+from math import pi, sqrt, gamma, factorial
+
+
+def closed(n):
+    return n * sqrt(pi) ** n / gamma(n / 2 + 1)
+
+
+def cases(n):
+    if n % 2 == 0:
+        n2 = n // 2
+        return n * pi ** n2 / factorial(n2)
+
+    return (
+        n
+        * pi ** ((n - 1) // 2)
+        * 2 ** (n + 1)
+        * factorial((n + 1) // 2)
+        / factorial(n + 1)
+    )
+
+
+def recurrence(n):
+    assert n >= 1
+    if n == 1:
+        return 2
+    elif n == 2:
+        return 2 * pi
+    return recurrence(n - 2) * 2 * pi / (n - 2)
+
+
+@pytest.mark.parametrize("n", range(1, 10))
+def test(n):
+    ref = closed(n)
+    tol = 1.0e-14
+    assert abs(ref - cases(n)) < abs(ref) * tol
+    assert abs(ref - recurrence(n)) < abs(ref) * tol
