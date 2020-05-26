e3002771ef85e15cba1490935ac18e387ac8c292
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-26 19:56:46 +0200

    separate out all

---
+++ test/test_chebyshev2.py
@@ -0,0 +1,36 @@
+"""
+\\int_-1^1 x^k * sqrt(1 - x^2)
+"""
+from math import sqrt, pi, gamma, factorial
+import pytest
+
+
+def closed(k):
+    return (
+        ((-1) ** k + 1)
+        / 2
+        * (sqrt(pi) * gamma((k + 1) / 2) / ((k + 2) * gamma(k / 2 + 1)))
+    )
+
+
+def cases(k):
+    if k % 2 == 0:
+        return pi * factorial(k) / (2 ** k * (k + 2) * factorial(k / 2) ** 2)
+    return 0
+
+
+def recurrence(k):
+    assert k >= 0
+    if k == 0:
+        return pi / 2
+    elif k == 1:
+        return 0
+    return recurrence(k - 2) * (k - 1) / (k + 2)
+
+
+@pytest.mark.parametrize("n", range(10))
+def test(n):
+    ref = closed(n)
+    tol = 1.0e-14
+    assert abs(ref - cases(n)) < (1 + abs(ref)) * tol
+    assert abs(ref - recurrence(n)) < (1 + abs(ref)) * tol
