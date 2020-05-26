55aa555a729de90ad22f7eef012fb57f67d158e5
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-26 10:52:48 +0200

    add hermite polynomials

--- test/test.py
+++ test/test.py
@@ -109,5 +109,43 @@ def test_chebyshev2(k):
     assert abs(closed(k) - rec(k)) < (1 + closed(k)) * tol
 
 
+# \int_-inf^inf x^k * exp(-x^2)
+@pytest.mark.parametrize("k", range(10))
+def test_hermite_physicist(k):
+    def rec(k):
+        assert k >= 0
+        if k == 0:
+            return sqrt(pi)
+        elif k == 1:
+            return 0
+        return rec(k - 2) * (k - 1) / 2
+
+    def closed(k):
+        if k % 2 == 1:
+            return 0.0
+        return gamma((k + 1) / 2)
+
+    assert abs(closed(k) - rec(k)) < (1 + closed(k)) * tol
+
+
+# \int_-inf^inf x^k * exp(-x^2/2) / sqrt(2*pi)
+@pytest.mark.parametrize("k", range(10))
+def test_hermite_probabilist(k):
+    def rec(k):
+        assert k >= 0
+        if k == 0:
+            return 1
+        elif k == 1:
+            return 0
+        return rec(k - 2) * (k - 1)
+
+    def closed(k):
+        if k % 2 == 1:
+            return 0.0
+        return 2 ** ((k + 1) / 2) * gamma((k + 1) / 2) / sqrt(2 * pi)
+
+    assert abs(closed(k) - rec(k)) < (1 + closed(k)) * tol
+
+
 if __name__ == "__main__":
     test_nsphere(5)
