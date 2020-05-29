f7b7a602c6ad6225dd4ce067282328e9acfc70c6
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-29 19:45:41 +0200

    generalized laguerre in tests

--- test/test_enr.py
+++ test/test_enr.py
@@ -3,29 +3,38 @@ from math import factorial, gamma, pi, sqrt
 import pytest
 
 
-def closed(n):
-    return 2 * sqrt(pi) ** n * gamma(n) / gamma(n / 2)
+def closed(n, alpha):
+    return 2 * sqrt(pi) ** n * gamma(n + alpha) / gamma(n / 2)
 
 
-def cases(n):
+def cases(n, alpha):
     if n % 2 == 0:
-        return 2 * pi ** (n / 2) * factorial(n - 1) / factorial(n / 2 - 1)
+        return 2 * pi ** (n / 2) * gamma(n + alpha) / factorial(n / 2 - 1)
 
-    return pi ** ((n - 1) / 2) * 2 ** n * factorial((n - 1) / 2)
+    return (
+        pi ** ((n - 1) / 2)
+        * 2 ** n
+        * factorial((n - 1) / 2)
+        * gamma(n + alpha)
+        / factorial(n - 1)
+    )
 
 
-def recurrence(n):
+def recurrence(n, alpha):
     assert n >= 0
-    if n == 0:
-        return 1
-    elif n == 1:
-        return 2
-    return recurrence(n - 2) * 2 * pi * (n - 1)
+    if n == 1:
+        return 2 * gamma(1 + alpha)
+    elif n == 2:
+        return 2 * pi * gamma(2 + alpha)
+    return (
+        recurrence(n - 2, alpha) * 2 * pi * (n + alpha - 1) * (n + alpha - 2) / (n - 2)
+    )
 
 
 @pytest.mark.parametrize("n", range(1, 10))
-def test(n):
-    ref = closed(n)
+@pytest.mark.parametrize("alpha", [0, 1, 1.5])
+def test(n, alpha):
+    ref = closed(n, alpha)
     tol = 1.0e-14
-    assert abs(ref - cases(n)) < abs(ref) * tol
-    assert abs(ref - recurrence(n)) < abs(ref) * tol
+    assert abs(ref - cases(n, alpha)) < abs(ref) * tol
+    assert abs(ref - recurrence(n, alpha)) < abs(ref) * tol
