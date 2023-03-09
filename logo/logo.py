2ce36015ce2dcb6f6d6d8458735928384c7d97f9
nschloe <nschloe@users.noreply.github.com>
2023-03-09 20:28:40 +0000

    Deploying to main from @ sigma-py/ndim-dev@d07f0515421c1cd50bf8a96dbd175f85c95ec328 🚀

---
+++ logo/logo.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+from numpy import cos, pi, sin
+
+ax = plt.gca()
+
+n = 24
+r0 = 0.5
+r1 = 0.5
+for k in range(n):
+    center = (r0 * cos(2 * k * pi / n), r0 * sin(2 * k * pi / n))
+    circle1 = plt.Circle(center, r1, ec="#555", linewidth=2.0, fill=False)
+    ax.add_patch(circle1)
+
+plt.xlim(-2, 2)
+plt.ylim(-2, 2)
+ax.set_aspect("equal")
+plt.axis("off")
+
+plt.savefig("logo.svg", transparent=True, bbox_inches="tight")
